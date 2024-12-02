6ad79f94b0cda3e0a62887e7678b4ca50787c99c
Alison Jenkins <alison.jenkins@brambles.com>
2024-12-02 11:04:19 +0000

    Day 2 part 2 Rust attempt (tests pass but doesn't work properly)

--- 2024/day2/advent-of-code-2024-day2/src/bin/day2.rs
+++ 2024/day2/advent-of-code-2024-day2/src/bin/day2.rs
@@ -9,20 +9,22 @@ use color_eyre::eyre::Result;
 fn main() -> Result<()> {
     color_eyre::install()?;
 
-    // read / parse data
     let input_file_path = PathBuf::from("../day2-data.txt");
     let data = parse_data(&input_file_path)?;
 
-    // process data
     let mut safe_records = 0;
+    let mut problem_dampener_safe_records = 0;
     for record in data {
         if process::are_readings_safe(&record) == Safety::Safe {
             safe_records += 1;
         }
+        if process::problem_dampener_safe_records(&record) == Safety::Safe {
+            problem_dampener_safe_records += 1;
+        }
     }
 
-    // output data
     println!("Safe records: {safe_records}");
+    println!("Problem Dampener safe records: {problem_dampener_safe_records}");
 
     Ok(())
 }
--- 2024/day2/advent-of-code-2024-day2/src/process.rs
+++ 2024/day2/advent-of-code-2024-day2/src/process.rs
@@ -53,6 +53,63 @@ pub fn are_readings_safe(readings: &Vec<i32>) -> Safety {
     Safety::Safe
 }
 
+pub fn problem_dampener_safe_records(readings: &Vec<i32>) -> Safety {
+    let mut prev_reading: Option<i32> = None;
+    let mut trend: Option<DataTrend> = None;
+    let mut problem_previously_dampened = false;
+
+    for reading in readings {
+        // get the last reading, if not set skip to the next reading after setting previous
+        let last_reading = if let Some(prev_reading) = prev_reading {
+            prev_reading
+        } else {
+            prev_reading = Some(*reading);
+            continue;
+        };
+
+        // Check the trend. If unset set it, if set check if the current data matches the trend
+        if last_reading < *reading && trend.is_none() {
+            trend = Some(DataTrend::Increasing);
+        } else if last_reading > *reading && trend.is_none() {
+            trend = Some(DataTrend::Decreasing);
+        } else if (last_reading > *reading && trend == Some(DataTrend::Increasing)
+            || last_reading < *reading && trend == Some(DataTrend::Decreasing))
+            && !problem_dampened(&mut problem_previously_dampened)
+        {
+            return Safety::Unsafe;
+        }
+
+        // Check if the data has changed... if not unsafe
+        if *reading == last_reading && !problem_dampened(&mut problem_previously_dampened) {
+            return Safety::Unsafe;
+        }
+
+        // Check if difference is too great
+        let diff = if last_reading < *reading {
+            *reading - last_reading
+        } else {
+            last_reading - *reading
+        };
+
+        if diff > 3 && problem_dampened(&mut problem_previously_dampened) {
+            return Safety::Unsafe;
+        }
+
+        prev_reading = Some(*reading);
+    }
+
+    Safety::Safe
+}
+
+fn problem_dampened(has_been_used: &mut bool) -> bool {
+    if *has_been_used {
+        return false;
+    }
+
+    *has_been_used = true;
+    true
+}
+
 #[cfg(test)]
 mod test {
     use std::path::PathBuf;
@@ -71,6 +128,39 @@ mod test {
         assert_eq!(are_readings_safe(&records[3]), Safety::Unsafe);
         assert_eq!(are_readings_safe(&records[4]), Safety::Unsafe);
         assert_eq!(are_readings_safe(&records[5]), Safety::Safe);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_problem_dampened() -> Result<()> {
+        let mut problem_previously_dampened = false;
+        let output = problem_dampened(&mut problem_previously_dampened);
+
+        // previously dampened now true
+        assert!(problem_previously_dampened);
+
+        // problem was dampened
+        assert!(output);
+
+        let output = problem_dampened(&mut problem_previously_dampened);
+
+        // problem not damepend if previously dampened
+        assert!(!output);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_problem_dampener_safe_records() -> Result<()> {
+        let records = parse_data(&PathBuf::from("../day-2-2-test-data.txt"))?;
+
+        assert_eq!(problem_dampener_safe_records(&records[0]), Safety::Safe);
+        assert_eq!(problem_dampener_safe_records(&records[1]), Safety::Unsafe);
+        assert_eq!(problem_dampener_safe_records(&records[2]), Safety::Unsafe);
+        assert_eq!(problem_dampener_safe_records(&records[3]), Safety::Safe);
+        assert_eq!(problem_dampener_safe_records(&records[4]), Safety::Safe);
+        assert_eq!(problem_dampener_safe_records(&records[5]), Safety::Safe);
 
         Ok(())
     }
