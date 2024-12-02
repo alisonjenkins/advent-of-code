44d7dd185af13da82af750d35bd24d86c15ca246
Alison Jenkins <alison.jenkins@brambles.com>
2024-12-02 09:49:09 +0000

    Working part 2 in Rust

--- 2024/day2/advent-of-code-2024-day2/src/bin/day2.rs
+++ 2024/day2/advent-of-code-2024-day2/src/bin/day2.rs
@@ -1,19 +1,28 @@
 use std::path::PathBuf;
 
-use advent_of_code_2024_day2::parse::parse_data;
+use advent_of_code_2024_day2::{
+    parse::parse_data,
+    process::{self, Safety},
+};
 use color_eyre::eyre::Result;
 
 fn main() -> Result<()> {
     color_eyre::install()?;
 
     // read / parse data
-    let input_file_path = PathBuf::from("../day-2-1-test-data.txt");
+    let input_file_path = PathBuf::from("../day2-data.txt");
     let data = parse_data(&input_file_path)?;
-    println!("{data:?}");
 
     // process data
+    let mut safe_records = 0;
+    for record in data {
+        if process::are_readings_safe(&record) == Safety::Safe {
+            safe_records += 1;
+        }
+    }
 
     // output data
+    println!("Safe records: {safe_records}");
 
     Ok(())
 }
